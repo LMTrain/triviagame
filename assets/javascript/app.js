2d739cd9dbcfe1fa75651cd3e87d96dc80b83188
LM Train <layconmm@msn.com>
2019-05-07 05:08:35 -0500

    Add new background files, modify exist codes in js and add a code the do count down, modify the css files for more rows and columns

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,98 @@
 
-var timerT = 0;
+var timerT = 60;
+var intervalId;
 var i = 0;
-var computerChoices = ["Who invented the telephone?(Bell)", "Which nail grows fastest? (Middle)", "What temperature does water boil at? (100C)", "Who discovered penicillin? (Fleming)", "What Spanish artist said he would eat his wife when she died? (Dali)", "Who wrote Julius Caesar, Macbeth and Hamlet? (Shakespeare)", "What did the crocodile swallow in Peter Pan? (Alarm clock)", "Which German city is famous for the perfume it produces? (Cologne)", "When did the First World War start? (1914)", "What did Joseph Priestley discover in 1774? (Oxygen)", "Where is the smallest bone in the body? (ear)", "Which is the only mammal that can’t jump? (Elephant)", "What color is a panda? (Black and White)", "Who cut Van Gogh’s ear? (He did)", "Who painted the Mona Lisa? (Da Vinci)", "How many dots are there on two dice? (42)", "What horoscope sign has a crab? (Cancer)", "When did the Second World War end? (1945)", "What’s the real name of Siddhartha Gautama? (Buddha)", "Where was Christopher Columbus born? (Genoa)", "When did the American Civil War end? (1865)", "Who said E=mc2? (Einstein)", "Which planet is nearest the sun? (Mercury)", "Where are the Dolomites? (Italy)", "What’s the capital of Kenya? (Nairobi?", "Which is the largest ocean? (Pacific)", "What’s the capital of Honduras? (Tegucigalpa)", "What’s the capital of Ethiopia? (Addis Ababa)", "How many squares are there on a chess board? (64)", "How many prongs are there on a fork? (4)", "Who starts first in chess? (White)", "How many events are there in the decathlon? (10)", "What do you use to take a cork out of a bottle? (a corkscrew)", "What money do they use in Japan? (Yen)", "What’s the Hungarian word for pepper? (Paprika)", "Name the two main actors in “The Sting”. (Paul Newman and Robert Redford)", "What year did Elvis Presley die? (1977)", "Which Italian leader was terribly afraid of the evil eye? (Mussolini)", "What country gave Florida to the USA in 1891? (Spain)", "Who gave his name to the month of July? (Julius Caesar)", "When was Elvis’ first ever concert? (1954)", "Who is the main actor in “Cocktail”? (Tom Cruise)"];
+var computerQ = ["What process involves heating an ore to obtain a metal?", "What did the Montgolfier brothers invent?", "What type of elephant has got the biggest ears?", "Who invented the electric light bulb?", "What’s the smallest type of tree in the world?", "Who invented television?", "What activity other than jumping are kangaroos good at?", "What’s the capital of Ecuador?", "What colors make purple?", "What’s the hardest rock?", "How much does a liter of water weigh?", "What year was President Kennedy killed?", "Where does the American president live?", "What is the first letter on a typewriter?", "What was the nickname of President Duvalier of Haiti, who died in 1971?", "In 1816 which US state was admitted to the Union as the 20th state?", "Who discovered the vaccination against smallpox in 1796?", "Which is the oldest University in the USA?", "What, along with heart disease and cancer, accounts for 64 percent of U.S. deaths?", "Who invented the telephone?", "Which nail grows fastest?", "What temperature does water boil at?", "Who discovered penicillin?", "What Spanish artist said he would eat his wife when she died?", "Who wrote Julius Caesar, Macbeth and Hamlet?", "What did the crocodile swallow in Peter Pan?", "Which German city is famous for the perfume it produces?", "When did the First World War start?", "What did Joseph Priestley discover in 1774?", "Where is the smallest bone in the body?", "Which is the only mammal that can’t jump?", "What color is a panda?", "Who cut Van Gogh’s ear?", "Who painted the Mona Lisa?", "How many dots are there on two dice?", "What horoscope sign has a crab?", "When did the Second World War end?", "What’s the real name of Siddhartha Gautama?", "Where was Christopher Columbus born?", "When did the American Civil War end?", "Who said E=mc2 ?", "Which planet is nearest the sun?", "Where are the Dolomites?", "What’s the capital of Kenya?", "Which is the largest ocean?", "What’s the capital of Honduras?", "What’s the capital of Ethiopia?", "How many squares are there on a chess board?", "How many prongs are there on a fork?", "Who starts first in chess?", "How many events are there in the decathlon?", "What do you use to take a cork out of a bottle?", "What money do they use in Japan?", "What’s the Hungarian word for pepper?", "Name the two main actors in “The Sting”.", "What year did Elvis Presley die?", "Which Italian leader was terribly afraid of the evil eye?", "What country gave Florida to the USA in 1891?", "Who gave his name to the month of July?", "When was Elvis’ first ever concert?", "Who is the main actor in “Cocktail”?"];
+var computerA = ["Smelting", "The balloon", "African Elephant", "Thomas Edison", "Bonsai", "John Logie Baird", "Boxing", "Quito", "Red and Blue", "Diamond", "1kg or 2.2lbs", "1963", "The White House", "Q", "Papa Doc", "Mississippi", "Edward Jenner", "Harvard", "Stroke", "Bell", "Middle", "212F", "Fleming", "Dali", "Shakespeare", "Alarm clock", "Cologne", "1914", "Oxygen", "ear", "Elephant", "Black and White", "He did", "Da Vinci", "42", "Cancer", "1945", "Buddha", "Genoa", "1865", "Einstein", "Mercury", "Italy", "Nairobi", "Pacific", "Tegucigalpa", "Addis Ababa", "64", "4", "White", "10", "A Corkscrew", "Yen", "Paprika", "Paul Newman and Robert Redford", "1977", "Mussolini", "Spain", "Julius Caesar", "1954", "Tom Cruise"];
 var txt = " ";
+var n = 0;
 var speed = 50;
-txt = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+txt = computerQ[Math.floor(Math.random() * computerQ.length)];
+n = computerQ.indexOf(txt);
+txtAns = computerA[n];
 console.log("This is txt = " + txt);
-$("#trivia-head").text("General Trivia Questions");
+console.log("ComputerQ =  " + computerQ.length);
+console.log("ComputerA =  " + computerA.length);
+console.log("txt = " + txt.length);
+console.log("n =  " + n);
+console.log("ComputerA  =  " + txtAns);
+$("#trivia-head").text("General Trivia Questions!");
+
 
 function typeWriter() {
-    if (i < txt.length) {
-        document.getElementById("question").innerHTML += txt.charAt(i);
-        i++;
-        timerT++;
-        if (timerT > txt.length) {
-            gameOver()
-        }
-        setTimeout(typeWriter, speed);
-        $("#start-button").hide();
-
+  if (i < txt.length) {
+    document.getElementById("question").innerHTML += txt.charAt(i);
+    i++;
+    // console.log("txt = " + txt.length);
+    // timerT;
+    // if (timerT > txt.length) {
+    //   gameOver()
+    // }
+    setTimeout(typeWriter, speed);
+    $("#start-button").hide();
+    $("#trivia-ans").text(txtAns);
+    //  The decrement function.
+    function run() {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
     }
-  
-  
+    function decrement() {
+    
+      //  Decrease number by one.
+      timerT--;
+    
+      //  Show the number in the #show-number tag.
+      $("#start-time").html("Time : " + timerT );
+    
+    
+      //  Once number hits zero...
+      if (timerT === 0) {
+    
+        //  ...run the stop function.
+        stop();
+    
+        //  Alert the user that time is up.
+        console.log("GAME OVER");
+      }
+    }
+    
+     //  The stop function
+     function stop() {
+    
+      //  Clears our intervalId
+      //  We just pass the name of the interval
+      //  to the clearInterval function.
+      clearInterval(intervalId);
+    }
+    
+    //  Execute the run function.
+    run();
+  }
 }
 
+
+
 function myFunction() {
-    if (timerT >= 20) {
-        gameOver()
-      }
-  
-    }
-  
-  
-  
+  if (timerT >= 20) {
+    gameOver()
+  }
+
+}
+
+
+
 
 function gameOver() {
-    // document.getElementById("user-text-input").innerHTML = "GAME OVER!!!!!";
-    // document.getElementById("user-text").innerHTML = "GAME OVER!!!!!";
-    // document.getElementById("artist-is").style.visibility = 'hidden';
-    // document.getElementById("computer-guess-block").style.visibility = 'hidden';
-    document.getElementById("start-button").innerHTML = "CLICK TO RESTART";
-    $('#start-button').click(
-      function () {
-        location.reload(true);
-      }
-    )
-  };
+  // document.getElementById("user-text-input").innerHTML = "GAME OVER!!!!!";
+  // document.getElementById("user-text").innerHTML = "GAME OVER!!!!!";
+  // document.getElementById("artist-is").style.visibility = 'hidden';
+  // document.getElementById("computer-guess-block").style.visibility = 'hidden';
+  document.getElementById("start-button").innerHTML = "CLICK TO RESTART";
+  $('#start-button').click(
+    function () {
+      location.reload(true);
+    }
+  )
+};
 
 document.getElementById("start-button").innerHTML = "START";
