82b3939bf5b1e5f5126f5c872804c253a30d8aa6
LM Train <layconmm@msn.com>
2019-05-07 21:26:14 -0500

    Add more Objective answers to array and modify css file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,9 +4,9 @@ var intervalId;
 var i = 0;
 var computerQ = ["What process involves heating an ore to obtain a metal?", "What did the Montgolfier brothers invent?", "What type of elephant has got the biggest ears?", "Who invented the electric light bulb?", "What’s the smallest type of tree in the world?", "Who invented television?", "What activity other than jumping are kangaroos good at?", "What’s the capital of Ecuador?", "What colors make purple?", "What’s the hardest rock?", "How much does a liter of water weigh?", "What year was President Kennedy killed?", "Where does the American president live?", "What is the first letter on a typewriter?", "What was the nickname of President Duvalier of Haiti, who died in 1971?", "In 1816 which US state was admitted to the Union as the 20th state?", "Who discovered the vaccination against smallpox in 1796?", "Which is the oldest University in the USA?", "What, along with heart disease and cancer, accounts for 64 percent of U.S. deaths?", "Who invented the telephone?", "Which nail grows fastest?", "What temperature does water boil at?", "Who discovered penicillin?", "What Spanish artist said he would eat his wife when she died?", "Who wrote Julius Caesar, Macbeth and Hamlet?", "What did the crocodile swallow in Peter Pan?", "Which German city is famous for the perfume it produces?", "When did the First World War start?", "What did Joseph Priestley discover in 1774?", "Where is the smallest bone in the body?", "Which is the only mammal that can’t jump?", "What color is a panda?", "Who cut Van Gogh’s ear?", "Who painted the Mona Lisa?", "How many dots are there on two dice?", "What horoscope sign has a crab?", "When did the Second World War end?", "What’s the real name of Siddhartha Gautama?", "Where was Christopher Columbus born?", "When did the American Civil War end?", "Who said E=mc2 ?", "Which planet is nearest the sun?", "Where are the Dolomites?", "What’s the capital of Kenya?", "Which is the largest ocean?", "What’s the capital of Honduras?", "What’s the capital of Ethiopia?", "How many squares are there on a chess board?", "How many prongs are there on a fork?", "Who starts first in chess?", "How many events are there in the decathlon?", "What do you use to take a cork out of a bottle?", "What money do they use in Japan?", "What’s the Hungarian word for pepper?", "Name the two main actors in “The Sting”.", "What year did Elvis Presley die?", "Which Italian leader was terribly afraid of the evil eye?", "What country gave Florida to the USA in 1891?", "Who gave his name to the month of July?", "When was Elvis’ first ever concert?", "Who is the main actor in “Cocktail”?"];
 var computerA = ["Smelting", "The Balloon", "African Elephant", "Thomas Edison", "Bonsai", "John Logie Baird", "Boxing", "Quito", "Red and Blue", "Diamond", "1kg or 2.2lbs", "1963", "The White House", "Q", "Papa Doc", "Mississippi", "Edward Jenner", "Harvard", "Stroke", "Bell", "Middle", "212F", "Fleming", "Dali", "Shakespeare", "Alarm clock", "Cologne", "1914", "Oxygen", "ear", "Elephant", "Black and White", "He did", "Da Vinci", "42", "Cancer", "1945", "Buddha", "Genoa", "1865", "Einstein", "Mercury", "Italy", "Nairobi", "Pacific", "Tegucigalpa", "Addis Ababa", "64", "4", "White", "10", "A Corkscrew", "Yen", "Paprika", "Paul Newman and Robert Redford", "1977", "Mussolini", "Spain", "Julius Caesar", "1954", "Tom Cruise"];
-var computerObj1 = ["Boiling", "The Computer", "Asian Elephant", "Sir Isaac Newton", "Banyan", "John Wilkinson", "Running", "Buenos Aires", "Blue and Green", "Talc", "1.5kg or 3.2lbs", "1965", "The Pentagon", "W", "Dada Luis", "Georgia", "Edward Jenner", "Harvard", "Stroke", "Bell", "Middle", "212F", "Fleming", "Dali", "Shakespeare", "Alarm clock", "Cologne", "1914", "Oxygen", "ear", "Elephant", "Black and White", "He did", "Da Vinci", "42", "Cancer", "1945", "Buddha", "Genoa", "1865", "Einstein", "Mercury", "Italy", "Nairobi", "Pacific", "Tegucigalpa", "Addis Ababa", "64", "4", "White", "10", "A Corkscrew", "Yen", "Paprika", "Paul Newman and Robert Redford", "1977", "Mussolini", "Spain", "Julius Caesar", "1954", "Tom Cruise"];
-var computerObj2 = ["Vaporization", "Submarines", "Sub-Saharan Elephant", "Thomas Newcomen", "Peepal", "Charles Babbage", "Kicking", "Sucre", "Red and Yellow", "Corundum", "0.5kg or 1lb", "1967", "The Senate Building", "Y", "The Doc", "Louisiana", "Edward Jenner", "Harvard", "Stroke", "Bell", "Middle", "212F", "Fleming", "Dali", "Shakespeare", "Alarm clock", "Cologne", "1914", "Oxygen", "ear", "Elephant", "Black and White", "He did", "Da Vinci", "42", "Cancer", "1945", "Buddha", "Genoa", "1865", "Einstein", "Mercury", "Italy", "Nairobi", "Pacific", "Tegucigalpa", "Addis Ababa", "64", "4", "White", "10", "A Corkscrew", "Yen", "Paprika", "Paul Newman and Robert Redford", "1977", "Mussolini", "Spain", "Julius Caesar", "1954", "Tom Cruise"];
-var computerObj3 = ["Liquidation", "The Museum", " Ecuador Elephant", "William Cullen", "Eucalyptus", "Samuel Morse", "Walking", "Santiago", "Yellow and Blue", "Topaz", "2kg or 4.2lbs", "1961", "The Capital", "R", "Papi", "Texas", "Edward Jenner", "Harvard", "Stroke", "Bell", "Middle", "212F", "Fleming", "Dali", "Shakespeare", "Alarm clock", "Cologne", "1914", "Oxygen", "ear", "Elephant", "Black and White", "He did", "Da Vinci", "42", "Cancer", "1945", "Buddha", "Genoa", "1865", "Einstein", "Mercury", "Italy", "Nairobi", "Pacific", "Tegucigalpa", "Addis Ababa", "64", "4", "White", "10", "A Corkscrew", "Yen", "Paprika", "Paul Newman and Robert Redford", "1977", "Mussolini", "Spain", "Julius Caesar", "1954", "Tom Cruise"];
+var computerObj1 = ["Boiling", "The Computer", "Asian Elephant", "Sir Isaac Newton", "Banyan", "John Wilkinson", "Running", "Buenos Aires", "Blue and Green", "Talc", "1.5kg or 3.2lbs", "1965", "The Pentagon", "W", "Dada Luis", "Georgia", "Jethro Tull", "Harvard", "Stroke", "Bell", "Middle", "212F", "Fleming", "Dali", "Shakespeare", "Alarm clock", "Cologne", "1914", "Oxygen", "ear", "Elephant", "Black and White", "He did", "Da Vinci", "42", "Cancer", "1945", "Buddha", "Genoa", "1865", "Einstein", "Mercury", "Italy", "Nairobi", "Pacific", "Tegucigalpa", "Addis Ababa", "64", "4", "White", "10", "A Corkscrew", "Yen", "Paprika", "Paul Newman and Robert Redford", "1977", "Mussolini", "Spain", "Julius Caesar", "1954", "Tom Cruise"];
+var computerObj2 = ["Vaporization", "Submarines", "Sub-Saharan Elephant", "Thomas Newcomen", "Peepal", "Charles Babbage", "Kicking", "Sucre", "Red and Yellow", "Corundum", "0.5kg or 1lb", "1967", "The Senate Building", "Y", "The Doc", "Louisiana", "Abraham Darby", "Harvard", "Stroke", "Bell", "Middle", "212F", "Fleming", "Dali", "Shakespeare", "Alarm clock", "Cologne", "1914", "Oxygen", "ear", "Elephant", "Black and White", "He did", "Da Vinci", "42", "Cancer", "1945", "Buddha", "Genoa", "1865", "Einstein", "Mercury", "Italy", "Nairobi", "Pacific", "Tegucigalpa", "Addis Ababa", "64", "4", "White", "10", "A Corkscrew", "Yen", "Paprika", "Paul Newman and Robert Redford", "1977", "Mussolini", "Spain", "Julius Caesar", "1954", "Tom Cruise"];
+var computerObj3 = ["Liquidation", "The Museum", " Ecuador Elephant", "William Cullen", "Eucalyptus", "Samuel Morse", "Walking", "Santiago", "Yellow and Blue", "Topaz", "2kg or 4.2lbs", "1961", "The Capital", "R", "Papi", "Texas", "Thomas Savery", "Harvard", "Stroke", "Bell", "Middle", "212F", "Fleming", "Dali", "Shakespeare", "Alarm clock", "Cologne", "1914", "Oxygen", "ear", "Elephant", "Black and White", "He did", "Da Vinci", "42", "Cancer", "1945", "Buddha", "Genoa", "1865", "Einstein", "Mercury", "Italy", "Nairobi", "Pacific", "Tegucigalpa", "Addis Ababa", "64", "4", "White", "10", "A Corkscrew", "Yen", "Paprika", "Paul Newman and Robert Redford", "1977", "Mussolini", "Spain", "Julius Caesar", "1954", "Tom Cruise"];
 var txt = " ";
 var txtObj = " ";
 var n = 0;
@@ -142,6 +142,7 @@ function typeWriter() {
 //   }
 // )
 
+$("#objectivea-button").click(objectiveA)
 function objectiveA() {
   if (objA === txtAns) {
     document.getElementById("result").innerHTML = "Good Job!";
@@ -150,6 +151,7 @@ function objectiveA() {
   }
 
 }
+$("#objectiveb-button").click(objectiveB)
 function objectiveB() {
   if (objB === txtAns) {
     document.getElementById("result").innerHTML = "Good Job!";
@@ -158,7 +160,7 @@ function objectiveB() {
   }
 
 }
-
+$("#objectivec-button").click(objectiveC)
 function objectiveC() {
   if (objC === txtAns) {
     document.getElementById("result").innerHTML = "Good Job!";
@@ -167,7 +169,7 @@ function objectiveC() {
   }
 
 }
-
+$("#objectived-button").click(objectiveD)
 function objectiveD() {
   if (objD === txtAns) {
     document.getElementById("result").innerHTML = "Good Job!";
